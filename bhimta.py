01bae2e59b441360dc7bdd25e6df28702f16aafb
Hwrsh <112798811+Hwrsh@users.noreply.github.com>
2023-09-04 09:49:38 +0530

    Add files via upload

---
+++ bhimta.py
@@ -0,0 +1,96 @@
+import datetime 
+from time import sleep
+import os
+print("\033[1;36;40m", end = "") 
+logo = '''
+
+
+
+  /$$$$$$                      /$$                       /$$   /$$          
+ /$$__  $$                    | $$                      | $$  /$$/          
+| $$  \ $$ /$$   /$$  /$$$$$$$| $$$$$$$  /$$   /$$      | $$ /$$/   /$$$$$$ 
+| $$$$$$$$| $$  | $$ /$$_____/| $$__  $$| $$  | $$      | $$$$$/   |____  $$
+| $$__  $$| $$  | $$|  $$$$$$ | $$  \ $$| $$  | $$      | $$  $$    /$$$$$$$
+| $$  | $$| $$  | $$ \____  $$| $$  | $$| $$  | $$      | $$\  $$  /$$__  $$
+| $$  | $$|  $$$$$$$ /$$$$$$$/| $$  | $$|  $$$$$$/      | $$ \  $$|  $$$$$$$
+|__/  |__/ \____  $$|_______/ |__/  |__/ \______/       |__/  \__/ \_______/
+           /$$  | $$                                                        
+          |  $$$$$$/                                                        
+           \______/                                                         
+
+$$$$$$$\  $$\                                 $$\           
+$$  __$$\ $$ |                                $$ |          
+$$ |  $$ |$$$$$$$\   $$$$$$\   $$$$$$$\  $$$$$$$ | $$$$$$\  
+$$$$$$$\ |$$  __$$\ $$  __$$\ $$  _____|$$  __$$ | \____$$\ 
+$$  __$$\ $$ |  $$ |$$ /  $$ |\$$$$$$\  $$ /  $$ | $$$$$$$ |
+$$ |  $$ |$$ |  $$ |$$ |  $$ | \____$$\ $$ |  $$ |$$  __$$ |
+$$$$$$$  |$$ |  $$ |\$$$$$$  |$$$$$$$  |\$$$$$$$ |\$$$$$$$ |
+\_______/ \__|  \__| \______/ \_______/  \_______| \_______|
+                                                            
+
+$$$$$$$\            $$\ $$\                     
+$$  __$$\           $$ |$$ |                    
+$$ |  $$ |$$\   $$\ $$ |$$ | $$$$$$\  $$\   $$\ 
+$$$$$$$  |$$ |  $$ |$$ |$$ |$$  __$$\ \$$\ $$  |
+$$  __$$< $$ |  $$ |$$ |$$ |$$$$$$$$ | \$$$$  / 
+$$ |  $$ |$$ |  $$ |$$ |$$ |$$   ____| $$  $$<  
+$$ |  $$ |\$$$$$$  |$$ |$$ |\$$$$$$$\ $$  /\$$\ 
+\__|  \__| \______/ \__|\__| \_______|\__/  \__|                                                                                                                                                                                                                                                                       '''                                                       
+                                                        
+                                                        
+                                                                
+os.system('clear')                                                                 
+                                                               
+print(logo)                                                           
+
+print("\033[1;33;40m", end = "")
+
+print("--------------------------------------------------------------------- \n")
+
+print("\033[1;32;40m", end = "")
+
+print ('[==[ Tool Create By Manzar Ki Randi Bahen Zalima Urf Ayushu Randi ]==]\n')
+
+print ('[==[ Kothe Pr Chudne Wali Famous Girl Ayshu ]==] \n')
+
+
+print("\033[1;33;40m", end = "")
+
+print("-------------------------------------------------------------------- \n")
+
+print("\033[1;36;40m", end = "")
+
+ 
+print("\033[1;34;40m", end = "")
+ 
+ab=str(input("--> Ayshu Ke Kale Bhosde Me Cookies File Dalo Or I'd Login Done  :\033[1;32m "))
+ 
+print('\n')
+
+print("\033[1;35;40m", end = "")
+
+print('--->> Account Successfully Login Ho Gaya Manzar Ki behne Ki chut me')
+
+print('\n')
+
+print("\033[1;36;40m", end = "")
+
+bc=str(input("--> Ayashu Randi Ki chudai Ka Link Dalo : "))
+
+emailx2=str(input("--> Notepad Enter Kro Jo Direct Ayshu Urf Zalima Ki Chut M Gus Jaye : "))
+
+msg8= int(input("--> Ayshu Ki Chut M Kitni Der M Comments Jaye Wo Time Dalo : "))
+
+print('\n')
+
+
+np="Manzar Ko Dhokha deke dusro ka lund chat kr sabse chut mrvane wali randi ayshu urf zalima queen "
+
+print("\033[1;32;40m", end = "")
+
+f = datetime.datetime.now()
+print(" --> Ayshu Ke Bhosde Ka Link  :--", bc)
+print (f.strftime("--> KALI CHUT WALI AYUSHI RANDIKI RULLEX :D | | Date :: %d-%m-%Y  TIME :: %I:%M:%S %p"))
+print("-->> AUR YE AYSHU KI CHUT ME COMMENTS GUS GAYA  ::-", np, "\n")
+sleep(30)
+    
